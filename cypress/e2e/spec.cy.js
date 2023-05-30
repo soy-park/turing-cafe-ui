15e70348581719c50d5967851c12d260d806a121
Sara Park <sspark106@gmail.com>
2023-05-30 12:24:14 -0600

    refac: Delete comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,3 @@
-// Write tests covering what should be displayed on the page when the user first visits.
-// Write a test that checks that when data is put into the form, the value is reflected in that form input.
-// Write a test to check the user flow of adding a new reservation to the page.
-
 describe('Cafe Reservations', () => {
   
   beforeEach(() => {
